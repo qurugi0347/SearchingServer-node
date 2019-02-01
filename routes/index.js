60b777e3011d872dd77789d05624e95d2e904f01
jun <qurugi0347@gmail.com>
2019-02-01 17:21:36 +0900

    uuid check

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid/v1');
+const val=uuid();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.send(val);
 });
 
 module.exports = router;
