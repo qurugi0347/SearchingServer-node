0a905b8f175fa976defc59f912241f06bdad17ba
jun <qurugi0347@gmail.com>
2019-02-07 10:12:07 +0900

    search page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const uuid = require('uuid/v1');
+const path = require('path');
 const val=uuid();
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send(val);
+  console.log(path.join(__dirname+'/../views/search.html'));
+  //res.send(path.join(__dirname+'/../views/search.html'));
+  //res.send(val);
+  res.sendfile(path.join(__dirname+'/../views/search.html'));
 });
 
 module.exports = router;
